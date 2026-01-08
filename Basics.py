5a6169774f359e4655299260238d57452427cc1a
SyedSalahuddin1 <humbleakh21@gmail.com>
2026-01-08 06:57:02 +0530

    First Commit

---
+++ Basics.py
@@ -0,0 +1,49 @@
+# # Data Types
+
+# 1.int
+# 2.float
+# 3.str 
+# 4.bool
+# 5.list
+# 6.set
+# 7.dict
+# 8.tuple
+# complex
+# bin()
+print(bin(10))
+print(int('0b101', 2))
+
+# # Classes -> Custom types
+
+# Speacilized Data Types
+# Modules 
+
+# None
+
+# 1
+print(type(2+4))
+print(2-4)
+print(2 * 4)
+print(type(3.5+4.7))
+print(2**4)
+
+# math functions
+print(round(2.89))
+print(abs(-39))
+
+print((5 + 4) * 10 / 2)
+
+print(((5 + 4) * 10) / 2)
+
+print((5 + 4) * (10 / 2))
+
+print(5 + (4 * 10) / 2)
+
+print(5 + 4 * 10 // 2)
+
+# # augmented assignment operator
+# += 
+# *=
+# /=
+# -=
+
